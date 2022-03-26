ae478b8bc581eba4307b2c8a9b87c3069cc30c02
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-03-26 09:03:20 +0530

    add method to parse basic auth string

--- backend/backend/lib/util.py
+++ backend/backend/lib/util.py
@@ -1,5 +1,7 @@
 from typing import Optional
+from fastapi import Header
 from fastapi.responses import JSONResponse
+from base64 import b64decode
 
 
 class HTTPError(Exception):
@@ -19,3 +21,21 @@ class HTTPError(Exception):
             ok=False,
             **self.data
         ), status_code=self.status_code, headers=self.headers)
+
+
+def parse_basic_auth_header(header: Optional[str]) -> Optional[dict]:
+    if not header:
+        return None
+    if header.startswith("Basic "):
+        header = header.replace("Basic ", "")
+    else:
+        return None
+    try:
+        username, password = b64decode(header).decode("utf-8").split(":")
+        return dict(username=username, password=password)
+    except ValueError:
+        return None
+
+
+def parse_basic_auth_header_depend(authorization: Header(None)) -> Optional[dict]:
+    return parse_basic_auth_header(authorization)
