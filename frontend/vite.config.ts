b94b8343ddbbec81dbd5c4b9bbc80b24075e10ed
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-04-07 15:21:01 +0530

    only use proxy while in development

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,19 +3,24 @@ import { svelte } from "@sveltejs/vite-plugin-svelte"
 import { resolve } from "path"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [svelte()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-				changeOrigin: true
+export default defineConfig(({ mode }) => {
+	const proxy = mode === "development" ? 
+			{
+				"/api": {
+					target: "http://localhost:5000",
+					changeOrigin: true
+				}
+			} 
+			: undefined
+	return {
+		plugins: [svelte()],
+		server: {
+			proxy
+		},
+		resolve: {
+			alias: {
+				$lib: resolve("./src/lib")
 			}
-		}
-	},
-	resolve: {
-		alias: {
-			$lib: resolve("./src/lib")
 		}
 	}
 })
