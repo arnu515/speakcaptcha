84ff156be0c7522efe255da42742da61e18c48c6
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-04-08 15:34:30 +0530

    add prose class

--- frontend/src/lib/layouts/documentation.svelte
+++ frontend/src/lib/layouts/documentation.svelte
@@ -1,1 +1,11 @@
-<slot />
+<script lang="ts">
+export let title: string
+export let summary: string
+</script>
+
+<div class="prose md:prose-lg lg:prose-xl max-w-screen-lg mx-auto p-8 my-8">
+	<h1 class="text-6xl font-bold mt-12 mb-4">{@html title}</h1>
+	<p class="text-2xl mb-4">{@html summary}</p>
+	<hr class="border-t border-gray-300 w-[70%] ml-4 my-8" />
+	<slot />
+</div>
