e45a0c3e433f465e6350a980b72afd179e418e97
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-04-08 19:47:54 +0530

    add

---
+++ frontend/src/lib/components/SpeakCaptchaDemo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+export let id: string
+</script>
+
+<iframe
+	src="/api/captcha?application_id={import.meta.env.VITE_SPEAKCAPTCHA_ID}#{id}"
+	frameborder="0"
+	title="SpeakCaptcha Challenge"
+	style="width: 100%;"
+	allow="microphone"
+	{id} />
