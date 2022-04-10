3ef1dbcc954cd5e2b7e83d2089fa8d7398a88a6d
arnu515 <arnu515@protonmail.com>
2022-04-10 05:53:27 +0000

    use https

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from starlette.middleware.sessions import SessionMiddleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 from .app import app
 from .dependencies.servedist import serve, handle_catch_all
@@ -8,6 +9,10 @@ from .dependencies.staticfiles import static
 from .lib.util import HTTPError
 from .routers.captcha import router as captcha
 from .routers.developers import router as developers
+
+dev = bool(int(os.getenv("DEV", "0")))
+if dev:
+    app.add_middleware(HTTPSRedirectMiddleware)
 
 app.include_router(developers, prefix='/api/developers')
 app.include_router(captcha, prefix='/api/captcha')
