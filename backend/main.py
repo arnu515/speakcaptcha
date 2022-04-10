88558974c3e36d30cfd6444103b3287c55912d8e
arnu515 <arnu515@protonmail.com>
2022-04-10 05:34:55 +0000

    allow prod

--- backend/main.py
+++ backend/main.py
@@ -3,5 +3,8 @@ import os
 import uvicorn
 
 if __name__ == '__main__':
+    print(os.getenv("PORT"), os.getenv("DEV"))
     port = int(os.getenv("PORT", "5000"))
-    uvicorn.run('backend:app', host='0.0.0.0', port=port, env_file=".env", reload=True)
+    dev = bool(int(os.getenv("DEV", "0")))
+    uvicorn.run('backend:app', host='0.0.0.0',
+                port=port, env_file=".env", reload=dev)
